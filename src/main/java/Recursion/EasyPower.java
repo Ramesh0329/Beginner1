b4711a45bd81a8a0651f8cee25d702fd3c0416b8
Ramesh0329 <rameshkoduru.java@gmail.com>
2023-01-23 22:35:28 +0530

    Initial Commit

--- src/main/java/Recursion/EasyPower.java
+++ src/main/java/Recursion/EasyPower.java
@@ -2,6 +2,23 @@ package Recursion;
 
 import java.util.Scanner;
 
+    /*
+        * Problem Description
+            You are given two integers A and B. You have to find the value of A^B.
+            NOTE: The value of A^B will always be less than or equal to 10^9.
+
+        * Problem Constraints
+            1 <= A, B <= 1000
+
+        * Input Format
+            First line of the input contains a single integer A.
+            Second line of the input contains a single integer B.
+
+        * Output Format
+            Print a single integer in single line.
+
+    */
+
 public class EasyPower {
     public static int power(int A, int B){
         if(B == 0){

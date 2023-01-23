272c0998eed54c54504f05fcf23edf939e0cea45
Ramesh0329 <rameshkoduru.java@gmail.com>
2023-01-23 22:20:45 +0530

    Initial Commit

--- src/main/java/Recursion/MultiplyTheNumbers.java
+++ src/main/java/Recursion/MultiplyTheNumbers.java
@@ -21,10 +21,13 @@ import java.util.Scanner;
 
 public class MultiplyTheNumbers {
     public static int product(int A, int B){
-        if(A == 0 || B == 0){
+        if(B == 0){
             return 0;
+        }
+        if(B == 1){
+            return A;
         }else{
-            return A * B;
+            return A + product(A, B-1);
         }
     }
     public static void main(String[] args) {
